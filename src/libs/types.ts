4555374ee81d62152654611732ca68bb3be31b68
phuwanat taratipprai <phuwanat@cmu.ac.th>
2024-08-07 15:47:01 +0700

    ed

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,3 +1,18 @@
 // add type for UserCard's Props
 
+interface UserCardProps {
+    name: string;
+    imgUrl: string;
+    address: string;
+    email: string;
+}
+export type { UserCardProps };
+
 // add type for UserCardDetail's Props
+
+interface UserCardDetailProps {
+    email: string;
+    address: string;
+}
+export type { UserCardDetailProps };
+
